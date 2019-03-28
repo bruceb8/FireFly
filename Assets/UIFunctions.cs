1776991b7b6fae8316751d4bb86a9c0755fce1da
Alex Boyle <alexboyle@D-10-16-14-227.dhcp4.washington.edu>
2019-03-28 14:53:14 -0700

    Added back button and center on device button

---
+++ Assets/UIFunctions.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIFunctions : MonoBehaviour
+{
+
+    public GameObject map;
+    private OnlineMaps m;
+    private OnlineMapsMarkerManager m_manager;
+    public WSNetworkManager WSManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m = map.GetComponent<OnlineMaps>();
+        m_manager = map.GetComponent<OnlineMapsMarkerManager>();
+    }
+
+    // Update is called once per frame
+    void Update(){}
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("menu", LoadSceneMode.Single);
+    }
+    public void CenterMapOnDevices()
+    {
+        if( m_manager.Count > 0) m.SetPosition(getAvgLon(), getAvgLat());
+     
+    }
+    private double getAvgLon()
+    {
+        double output = 0;
+        foreach (OnlineMapsMarker marker in m_manager.items)
+        {
+            output += marker.position.x;
+        }
+        return output / m_manager.Count;
+    }
+    private double getAvgLat()
+    {
+        double output = 0;
+        foreach (OnlineMapsMarker marker in m_manager.items)
+        {
+            output += marker.position.y;
+        }
+        return output / m_manager.Count;
+    }
+}
